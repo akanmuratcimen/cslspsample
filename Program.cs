19a9595f8014e2a43479dd7fbb69d3c303353433
akanmuratcimen <akan.murat.cimen@gmail.com>
2023-03-03 09:53:44 +0100

    initial

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿static class TestClass
+{
+    public static string TestMethod(string str) => str;
+}
+
+class Program
+{
+    private static void Main()
+    {
+        var testVariable = TestClass.TestMethod("test");
+
+        Console.WriteLine(testVariable);
+    }
+}
